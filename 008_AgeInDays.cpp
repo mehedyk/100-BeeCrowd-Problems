ca6691fef01328ecb91fd0941164b02e3fe03a92
S.M. Mehedy Kawser <mahdi.kawser@gmail.com>
2025-05-30 00:48:02 +0600

    Update 008_AgeInDays.cpp

--- 008_AgeInDays.cpp
+++ 008_AgeInDays.cpp
@@ -1,20 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int days,year=0,month=0;
+    int days,year=0,month=0,day=0;
     cin>>days;
 
-    if(days>=365) {
-        year=days/365;
-        days%=365;
-        if(days>=30){
-            month=days/30;
-            days=days%30;
-        }
-    }
-    else if(days>=30){
-        month=days/30;
-        days=days%30;
-    }
+    year=days/365;
+    days=days%365;
+    month=days/30;
+    days=days%30;
+
     printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n",year,month,days);
 }
