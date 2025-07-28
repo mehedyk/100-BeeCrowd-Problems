005356bd5e725d26630401c14925351bd9c4bab0
S.M. Mehedy Kawser <mahdi.kawser@gmail.com>
2025-07-28 21:53:07 +0600

    Create 009_Salary.cpp

---
+++ 009_Salary.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    double o;
+    cin>>n>>m>>o;
+    double p=m*o;
+    cout<<"NUMBER = "<<n<<endl;
+    printf("SALARY = U$ %.2f\n",p);
+}
