736dd4b3339200500f0a1ac1b0ddf1270995e4b4
mehedyk <mahdi.kawser@gmail.com>
2025-12-06 23:52:43 +0600

    adding 32

---
+++ 032_Game_Time.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,result=0;
+    cin>>a>>b;
+    if(a>!12 && b<a) {
+        a=24-a;
+        result=a+b;
+        cout<<"O JOGO DUROU "<<result<<" HORA(S)"<<endl;
+    }
+    else if(a==b){
+        cout<<"O JOGO DUROU 24 HORA(S)"<<endl;
+        return 0;
+    }
+    else if(a<b){
+        result=b-a;
+        cout<<"O JOGO DUROU "<<result<<" HORA(S)"<<endl;
+    }
+}
