e88914cf84ffbed9d29b962c4591d35997f0a97b
mehedyk <mahdi.kawser@gmail.com>
2026-04-09 18:21:04 +0600

    38

--- 038_EventTime.cpp
+++ 038_EventTime.cpp
@@ -1,13 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        if(n<60) cout<<n<<" segundo(s)"<<endl;
-        else if(n<3600) cout<<n/60<<" minuto(s) e "<<n%60<<" segundo(s)"<<endl;
-        else cout<<n/3600<<" hora(s), "<<(n%3600)/60<<" minuto(s) e "<<n%60<<" segundo(s)"<<endl;
-    }
+    string dia;
+    int d1,d2,h1,m1,s1,h2,m2,s2;
+    char c;
+    cin>>dia>>d1>>h1>>c>>m1>>c>>s1;
+    cin>>dia>>d2>>h2>>c>>m2>>c>>s2;
+
+    int total1=d1*86400+h1*3600+m1*60+s1;
+    int total2=d2*86400+h2*3600+m2*60+s2;
+    int diff=total2-total1;
+
+    int days=diff/86400; diff%=86400;
+    int hours=diff/3600; diff%=3600;
+    int mins=diff/60;
+    int secs=diff%60;
+
+    printf("%d dia(s)\n%d hora(s)\n%d minuto(s)\n%d segundo(s)\n",days,hours,mins,secs);
 }
