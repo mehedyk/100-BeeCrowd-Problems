4d69da89242a0a33af9a45af6f1aad50bd599796
mehedyk <mahdi.kawser@gmail.com>
2026-06-10 00:19:05 +0600

    055

--- 055_SequenceIJ4.cpp
+++ 055_SequenceIJ4.cpp
@@ -1,22 +1,14 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n,x,y;
-    cin>>n;
-    
-    for(int i=0;i<n;i++){
-        cin>>x>>y;
-        
-        if(x>y){
-            swap(x,y);
-        }
-        
-        int sum=0;
-        for(int j=x+1;j<y;j++){
-            if(j%2!=0){
-                sum+=j;
+    for(double i=0;i<=2.01;i+=0.2){
+        for(int k=0;k<3;k++){
+            double j=1+k+i;
+            if(fabs(i-0.0)<1e-9 || (i>=0.9 && i<=1.01) || i>=1.99){
+                printf("I=%d J=%d\n",(int)round(i),(int)round(j));
+            } else {
+                printf("I=%.1f J=%.1f\n",i,j);
             }
         }
-        cout<<sum<<endl;
     }
 }
