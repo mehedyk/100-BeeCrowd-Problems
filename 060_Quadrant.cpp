6221d692b69a351577bcad880d6ff40840e82599
mehedyk <mahdi.kawser@gmail.com>
2026-06-15 02:58:16 +0600

    060

--- 060_Quadrant.cpp
+++ 060_Quadrant.cpp
@@ -1,1 +1,16 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,x,y;
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+        cin >>x>>y;
+        if(y==0){
+            cout<<"divisao impossivel"<<endl;
+        }else{
+            double result=(double)x/y;
+            cout<<fixed<<setprecision(1)<<result<<endl;
+        }
+    }
+}
